fd2769255aef1bafd2440eef59d3711512533578
Xibin Liu <xibinliu@yahoo.com>
2019-07-21 16:54:36 -0700

    Model change to validate email and phone number of student

--- models/academics/student.js
+++ models/academics/student.js
@@ -2,22 +2,55 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
+const validatePhone = function(phone) {
+  var re = /^(\d+-?)+\d+$/;
+  return re.test(phone)
+}
+
+const validateEmail = function(email) {
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email)
+}
+
 var schema = new Schema(
   {
     first_name: {type: String, required: true, maxlength: 100},
     last_name: {type: String, required: true, maxlength: 100},
     date_of_birth: {type: Date},
-    email: {type: String, required: true, maxlength: 100},
-    phoneNumber: {type: String, required: true, maxlength: 20},
+    email: {
+      type: String, required: true, maxlength: 100,
+      trim: true,
+      lowercase: true,
+      validate: [validateEmail, 'Please fill a valid email address'],
+    },
+    phoneNumber: {
+      type: String, required: true, maxlength: 20,
+      trim: true,
+      validate: [validatePhone, 'Please fill a valid phone number']
+    },
     school: {type: String, required: true, maxlength: 100},
     grade: {type: Number, required: true},
     GuardianOneName: {type: String, required: false, maxlength: 100},
-    GuardianOnePhone: {type: String, required: false, maxlength: 20},
-    GuardianOneEmail: {type: String, required: false, maxlength: 100},
+    GuardianOnePhone: {type: String, required: false, maxlength: 20,
+      trim: true,
+      validate: [validatePhone, 'Please fill a valid phone number']
+    },
+    GuardianOneEmail: {type: String, required: false, maxlength: 100,
+      trim: true,
+      lowercase: true,
+      validate: [validateEmail, 'Please fill a valid email address'],
+    },
 
     GuardianTwoName: {type: String, required: false, maxlength: 100},
-    GuardianTwoPhone: {type: String, required: false, maxlength: 20},
-    GuardianTwoEmail: {type: String, required: false, maxlength: 100},
+    GuardianTwoPhone: {type: String, required: false, maxlength: 20,
+      trim: true,
+      validate: [validatePhone, 'Please fill a valid phone number']
+    },
+    GuardianTwoEmail: {type: String, required: false, maxlength: 100,
+      trim: true,
+      lowercase: true,
+      validate: [validateEmail, 'Please fill a valid email address'],
+    },
   },
   {
     timestamps: true
@@ -33,5 +66,21 @@ schema.virtual('name').get(function () {
   if (!fullName) fullName = "Unknown"
   return fullName;
 });
+
+const emailValidate = [
+  {validator: validateEmail, msg: "Please fill a valid email address."},
+];
+const phoneValidate = [
+  {validator: validatePhone, msg: "Please fill a valid phone number."},
+];
+const validators = {
+  "email": emailValidate,
+  "GuardianOneEmail": emailValidate,
+  "GuardianTwoEmail": emailValidate,
+  "phoneNumber": phoneValidate,
+  "GuardianOnePhone": phoneValidate,
+  "GuardianTwoPhone": phoneValidate,
+}
+schema.mddsValidators = validators;
 
 module.exports = schema;
