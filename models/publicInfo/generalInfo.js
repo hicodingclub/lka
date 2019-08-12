1756e8bcfc6363bc858a08cab6f02fbc6959bcef
Xibin Liu <xibinliu@yahoo.com>
2019-08-11 23:19:15 -0700

    release-20190811-1

--- models/publicInfo/generalInfo.js
+++ models/publicInfo/generalInfo.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const schema = new Schema(
   {
     title: {type: String, required: true},
-    description: {type: String, required: true, maxlength: 1000},
+    description: {type: String, required: true, maxlength: 1000, textarea: true, },
     signaturePicture: {type: String,  required: false,
       mraType: 'picture', mraSharable: true},
     tag: {type: String, required: false, maxlength: 50},
