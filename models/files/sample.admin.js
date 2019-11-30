e59262124e9efc2ff4d5e934af10a8512f16f935
Xibin Liu <xibinliu@yahoo.com>
2019-11-30 14:46:53 -0800

    File: show category counts

--- models/files/sample.admin.js
+++ models/files/sample.admin.js
@@ -30,6 +30,8 @@ const schemas = {
       listCategories: [
         {
           listCategoryField: 'group',
+          showCategoryCounts: true,
+          showEmptyCategory: false,
         },
       ],
      },
